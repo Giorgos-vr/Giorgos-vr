c01b1e2e1d991392f5c97e4e0a362d75e21bf1d7
Giorgos-vr <89841637+Giorgos-vr@users.noreply.github.com>
2021-09-05 18:37:58 +0300

    Update taxcalc.py

--- taxcalc.py
+++ taxcalc.py
@@ -4,8 +4,8 @@ print("This program will calculate yearly taxes due based on monthly income and 
 def taxfunc():                                                                    # function to calculate yearly tax
     monthly = (str(input("Monthly income: ")))                                    # input variable to be used later
     tax = (str(input("Tax percentage: ")))                                        # input variable to be used later
-    month = float(''.join(filter(str.isdigit, monthly)))                          # filter usable var from user input
-    taxfrac = float(''.join(filter(str.isdigit, tax)))                            # filter usable var from user input
+    month = float(''.join(filter(str.isdigit, monthly)))                          # filter usable data from user input
+    taxfrac = float(''.join(filter(str.isdigit, tax)))                            # filter usable data from user input
     taxdue = (month * 12) * taxfrac / 100                                         # calculate yearly tax (flat tax)
     print(f"Tax due is {taxdue} of your local currency")                          # RESULTS!!!
     repeat = (str(input('Would you like to make another calculation? Y/N: ')))    # repeat loop
